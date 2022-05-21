1641fad6cd3bbf72bdefd13d824ace2ab644a061
morgana ribeiro <morganfrime2017@gmail.com>
2022-05-21 20:57:38 -0300

    feat(home): create page home and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,38 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from 'next/head';
+import avatarSvg from '../../public/images/avatar.svg';
+import Image from 'next/image';
+import styles from './home.module.scss';
 
 export default function Home() {
+
   return (
     <>
       <Head>
-        <title> Ig.news </title>
+        <title> Home | Ig.news </title>
       </Head>
+
+      {/* Toda página precisa pelo menos de uma tag por volta de tudo, no caso a <main></main> */}
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span> 👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <div>
+          <Image
+            src={avatarSvg}
+            alt="Girl coding"
+            width="336"
+            height="521"
+            layout="fixed"
+          />
+        </div>
+      </main>
     </>
   )
 }
