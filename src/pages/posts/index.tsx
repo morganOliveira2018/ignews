c425cb00855e6d4779babb7ef2e650d42c1cc74c
Morgana Ribeiro <morganfrime2017@gmail.com>
2022-09-15 11:16:19 -0300

    feat(*): listing post on screen by prismic.io

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,65 @@
+import Head from 'next/head';
+import { getPrismicClient } from '../../services/prismic';
+import styles from './styles.module.scss';
+
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+};
+
+interface PostsProps {
+    posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
+    return (
+        <>
+            <Head>
+                <title> Posts | Ignews</title>
+            </Head>
+
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    {posts.map(post => (
+                        <a key={post.slug} href='#'>
+                            <time>{post.updatedAt}</time>
+                            <strong>{post.title}</strong>
+                            <p>{post.excerpt}</p>
+                        </a>
+                    ))}
+                </div>
+            </main>
+        </>
+    );
+}
+
+export async function getServerSideProps() {
+    const prismic = getPrismicClient()
+
+    const response = await prismic.getByType("post", {
+        fetch: ['post.title', 'post.content'],
+        pageSize: 100,
+    });
+
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: post.data.title,
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
+    })
+
+
+    console.log(JSON.stringify(posts, null, 2));
+
+    return {
+        props: { posts },
+    };
+}
